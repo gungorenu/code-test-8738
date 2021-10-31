18476be3a14059fb666b7c0c6fb82b9679931705
gungorenu <gungorenu@gmail.com>
2021-10-31 10:17:08 +0100

    initial project commit

---
+++ code-test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeTest
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("High Level Error! If you are seeing this most probably something big messed up");
+                Console.WriteLine("Error: {0}\r\nStacktrace: {1}", ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
